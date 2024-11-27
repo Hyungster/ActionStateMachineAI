8ba44dbb5af9635552f75cc20923f527e56378ca
Hyungster <131295827+Hyungster@users.noreply.github.com>
2024-11-27 16:34:48 -0500

    Basic State Timeline implemented

--- Assets/Objects/UI/UIManager.cs
+++ Assets/Objects/UI/UIManager.cs
@@ -5,49 +5,77 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    public float offset = 20;
+    public float scrollSpeed = 1;
+    public float verticalOffset = 1;
+    private int flip = 1;
+    public float offset = 0.5f;
 
     private GameObject canvasGO;
 
+    public TextMeshProUGUI templateStamp;
+    public GameObject stampHolder;
+    private RectTransform stampHolderTransform;
+    public RectTransform stampStartPosition;
+
+    public GameObject timeLine;
+
     public Character character;
-    private string currentText = "";
-    public TextMeshProUGUI stateTextTemplate;
-    private GameObject templateGO;
-    private RectTransform[] stateRects = new RectTransform[10];
-    private RectTransform[] rectTransforms = new RectTransform[10];
-    private List<string> statesEntered = new();
+    
 
     private void Awake()
     {
-        character.stateEntered.AddListener(PushTextListener);
+        character.stateEntered.AddListener(StampText);
 
-        canvasGO = stateTextTemplate.transform.parent.gameObject;
-        templateGO = stateTextTemplate.gameObject;
-        templateGO.SetActive(false);
-        
+        canvasGO = transform.GetChild(0).gameObject;
+
+        templateStamp.gameObject.SetActive(false);
+        stampHolderTransform = stampHolder.GetComponent<RectTransform>();
+        //StartCoroutine(TimeLineCoroutine());
     }
 
     private void Update()
     {
-        if (statesEntered.Count > 0)
-        {
+        stampHolderTransform.position += new Vector3(-scrollSpeed * Time.deltaTime, 0, 0);
+    }
 
+    private void StampText(string text)
+    {
+        GameObject stamp = Instantiate(templateStamp.gameObject, stampHolder.transform);
+        stamp.GetComponent<TextMeshProUGUI>().text = text;
+        stamp.SetActive(true);
+        stamp.transform.position = stampStartPosition.position;
+        stamp.transform.position += new Vector3(0, verticalOffset * flip, 0);
+        if (text.Equals("Scan")) stamp.transform.position -= new Vector3(this.offset, 0, 0);
+        flip *= -1;
+        Transform stampLine = stamp.transform.GetChild(0).transform;
+        Vector3 offset = new Vector3(stampLine.localPosition.x, stampLine.localPosition.y, 0);
+        if (flip > 0)
+        {
+            offset.y = -offset.y;
+        }
+        stampLine.localPosition = offset;
+    }
+
+    private IEnumerator TimeLineCoroutine()
+    {
+        float time = Time.time;
+        int currentBeat = 0;
+        for (; ; )
+        {
+            float scaledTime = time / character.beatDuration;
+            int calculatedBeat = Mathf.FloorToInt((float)scaledTime);
+            if (calculatedBeat > currentBeat)
+            {
+                SpawnTimeLine();
+                currentBeat = calculatedBeat;
+            }
+            time += Time.deltaTime;
+            yield return null;
         }
     }
-
-    private void PushTextListener(string text)
+    private void SpawnTimeLine()
     {
-        Debug.Log("ahahah");
-        statesEntered.Add(text);
-    }
-
-    private IEnumerator PushText(string text)
-    {
-        GameObject go = Instantiate(templateGO, templateGO.transform.position, Quaternion.identity);
-        go.transform.SetParent(canvasGO.transform, false);
-        go.GetComponent<TextMeshProUGUI>().text = text;
-        go.name = text;
-        go.SetActive(true);
-        yield return null;
+        GameObject timeLineClone = Instantiate(timeLine, stampHolderTransform);
+        timeLineClone.transform.position = stampStartPosition.position;
     }
 }
